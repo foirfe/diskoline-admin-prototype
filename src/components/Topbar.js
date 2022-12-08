26e534f9a57fc13dbd4911ad5f6ae69efe6d936a
Daníel Freyr <83401867+foirfe@users.noreply.github.com>
2022-12-09 00:09:05 +0100

    Dashboard page images and scss added

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,57 +1,67 @@
 import { signOut } from "firebase/auth";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { auth} from "../firebaseConfig";
+import { auth } from "../firebaseConfig";
 
 import logo from "../images/disko-line-emblem.png";
 import DropdownItem from "./DropdownItem";
 
-export default function Topbar(){
-const [username, setUsername] = useState("User");
-const [open, setOpen] = useState(false);
-const getauth = auth;
-const navigate = useNavigate();
-let menuRef = useRef();
+export default function Topbar() {
+  const [username, setUsername] = useState("User");
+  const [open, setOpen] = useState(false);
+  const getauth = auth;
+  const navigate = useNavigate();
+  let menuRef = useRef();
 
-useEffect(() => {
-  let handler = (e)=>{
-    if(!menuRef.current.contains(e.target)){
-      setOpen(false);
-    }      
-  };
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setOpen(false);
+      }
+    };
 
-  document.addEventListener("mousedown", handler);
-  
+    document.addEventListener("mousedown", handler);
 
-  return() =>{
-    document.removeEventListener("mousedown", handler);
-  }
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
 
-});
-
-
-function handleSignOut(){
+  function handleSignOut() {
     signOut(getauth);
     navigate("/");
-}
-function handleLogoNavigation(){
-    navigate("/");
-}
+  }
+  function handleLogoNavigation() {
+    navigate("/forside");
+  }
 
-    return(
-        <div className="topbar">
-            <img src={logo} alt="Site Logo" className="topbar-image" onClick={handleLogoNavigation}/>
-            
-            <div className='menu-container' ref={menuRef}>
-        <div className='menu-trigger' onClick={()=>{setOpen(!open)}}>
-          <p>{username}{open ? `▲` : `▼`}</p>
+  return (
+    <div className="topbar">
+      <img
+        src={logo}
+        alt="Site Logo"
+        className="topbar-image"
+        onClick={handleLogoNavigation}
+      />
+
+      <div className="menu-container" ref={menuRef}>
+        <div
+          className="menu-trigger"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          <p>
+            {username}
+            {open ? `▲` : `▼`}
+          </p>
         </div>
 
-        <div className={`dropdown-menu ${open? 'active' : 'inactive'}`} >
+        <div className={`dropdown-menu ${open ? "active" : "inactive"}`}>
           <ul>
-            <DropdownItem text={"Redigere bruger"}/>
+            <DropdownItem text={"Redigere bruger"} />
             <div onClick={handleSignOut}>
-            <DropdownItem  text={"Log Ud"}/>
+              <DropdownItem text={"Log Ud"} />
             </div>
           </ul>
         </div>
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,8 @@
 import { Helmet } from "react-helmet";
+import placeholder1 from "../images/Placeholder1.png";
+import placeholder2 from "../images/Placeholder2.png";
+import placeholder3 from "../images/Placeholder3.png";
+import placeholder4 from "../images/Placeholder4.png";
 
 export default function Dashboard() {
   return (
@@ -7,6 +11,27 @@ export default function Dashboard() {
         <title>Disko Line Admin</title>
       </Helmet>
       <h1>Dashboard</h1>
+      <p>Her vises der eksempel på hvordan en Dashboard side kunne se ud</p>
+      <div className="upper-dashboard">
+        <div>
+          <h2>Nyeste ændringer</h2>
+          <img src={placeholder1} alt="Placeholder 1" />
+        </div>
+        <div>
+          <h2>Seneste Logins</h2>
+          <img src={placeholder2} alt="Placeholder 2" />
+        </div>
+        <div>
+          <h2>Obs. Punkter</h2>
+          <img src={placeholder3} alt="Placeholder 3" />
+        </div>
+      </div>
+      <div className="lower-dashboard">
+        <div>
+          <h2>Salg af billetter for året</h2>
+          <img src={placeholder4} alt="Placeholder 4" />
+        </div>
+      </div>
     </div>
   );
 }
