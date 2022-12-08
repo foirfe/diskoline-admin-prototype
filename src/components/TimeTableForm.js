e1ec081835f781ddc6a3ec9c144a46d7b0db3c62
Daníel Freyr <83401867+foirfe@users.noreply.github.com>
2022-12-08 02:15:50 +0100

    Fixed date

--- src/components/TimeTableForm.js
+++ src/components/TimeTableForm.js
@@ -2,91 +2,106 @@ import { onSnapshot, query, where } from "@firebase/firestore";
 import { useEffect, useState } from "react";
 import { priceTablesRef } from "../firebaseConfig";
 
-export default function TimeTableForm({saveTimetable, timeTable}){
-    const [pricetables, setPricetables] = useState([]);
-    const [startDate, setStartDate] = useState();
-    const [endDate, setEndDate] = useState();
-    const [pricetable, setPricetable] = useState("");
-    const [pax, setPax] = useState(0);
-    const [errorMessage, setErrorMessage] = useState("");
-    useEffect(() => {
-        if (timeTable) {
-            setStartDate(timeTable.startDate);
-            setEndDate(timeTable.endDate);
-            setPricetable(timeTable.pricetable);
-            setPax(timeTable.pax);
-        }
-        async function getPriceTables(){
-            const q = query(
-                priceTablesRef,
-                where("area", "==", localStorage.getItem("area"))
-              );
-              const unsubscribe = onSnapshot(q, (data) => {
-                const priceTablesData = data.docs.map((doc) => {
-              
-                  return { ...doc.data(), id: doc.id }; 
-                });
-                setPricetables(priceTablesData);
-              });
-              return () => unsubscribe(); // tell the post component to unsubscribe from listen on changes from firestore
-        }
-        getPriceTables();
-    }, [timeTable]); // useEffect is called every time post changes.
-    function handleSubmit(event) {
-        event.preventDefault();
-      
-          const formData = {
-            // create a new objebt to hold the value from states / input fields
-            startDate: startDate,
-            endDate: endDate,
-            pricetable: pricetable,
-            pax: pax
-          };
-    
-          const validForm = formData.startDate && formData.endDate;
-          if (validForm) {
-            saveTimetable(formData);
-          } else {
-            setErrorMessage("Alle felter markeret med rød stjerne skal være udfyldte");
-          }
-        }
+export default function TimeTableForm({ saveTimetable, timeTable }) {
+  const [pricetables, setPricetables] = useState([]);
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [pricetable, setPricetable] = useState("");
+  const [pax, setPax] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
+  useEffect(() => {
+    if (timeTable) {
+      setStartDate(timeTable.startDate);
+      setEndDate(timeTable.endDate);
+      setPricetable(timeTable.pricetable);
+      setPax(timeTable.pax);
+    }
+    async function getPriceTables() {
+      const q = query(
+        priceTablesRef,
+        where("area", "==", localStorage.getItem("area"))
+      );
+      const unsubscribe = onSnapshot(q, (data) => {
+        const priceTablesData = data.docs.map((doc) => {
+          return { ...doc.data(), id: doc.id };
+        });
+        setPricetables(priceTablesData);
+      });
+      return () => unsubscribe(); // tell the post component to unsubscribe from listen on changes from firestore
+    }
+    getPriceTables();
+  }, [timeTable]); // useEffect is called every time post changes.
+  function handleSubmit(event) {
+    event.preventDefault();
 
-    return(
-        <form onSubmit={handleSubmit}>
-            <label>
-                Periode:<span></span>
-            </label>
-            <div className="startdate">
-            <p>Start dato:</p><input type="date" required  onChange={(e) => setStartDate(e.target.value)}/>
-            </div>
-            <div className="enddate">
-            <p>Slut dato:</p>
-            <input type="date" required onChange={(e) => setEndDate(e.target.value)}/>
-            </div>
-            <label>
-                Afgang-Ankomst
-            </label>
-            <label htmlFor="pricetable">
-                Pristabel
-            </label>
-            <select id="pricetable" name="pricetable" onChange={(e) => setPricetable(e.target.value)}>
-            {pricetables.map((pricetable) => (
-            <option key={pricetable.id} value={pricetable.name}>{pricetable.name}</option>
+    const formData = {
+      // create a new objebt to hold the value from states / input fields
+      startDate: startDate,
+      endDate: endDate,
+      pricetable: pricetable,
+      pax: pax,
+    };
+
+    const validForm = formData.startDate && formData.endDate;
+    if (validForm) {
+      saveTimetable(formData);
+    } else {
+      setErrorMessage(
+        "Alle felter markeret med rød stjerne skal være udfyldte"
+      );
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Periode:<span></span>
+      </label>
+      <div className="startdate">
+        <p>Start dato:</p>
+        <input
+          type="date"
+          required
+          onChange={(e) => setStartDate(e.target.value)}
+        />
+      </div>
+      <div className="enddate">
+        <p>Slut dato:</p>
+        <input
+          type="date"
+          required
+          onChange={(e) => setEndDate(e.target.value)}
+        />
+      </div>
+      <label>Afgang-Ankomst</label>
+      <label htmlFor="pricetable">Pristabel</label>
+      <select
+        id="pricetable"
+        name="pricetable"
+        onChange={(e) => setPricetable(e.target.value)}
+      >
+        {pricetables.map((pricetable) => (
+          <option key={pricetable.id} value={pricetable.name}>
+            {pricetable.name}
+          </option>
         ))}
-            </select>
-            <h3>Pax</h3>
-            <label>Antal Pax</label>
-            <input type="number" min="1" max="100" onChange={(e) => setPax(e.target.value)}/>
-            <h3>Tilkøb</h3>
-            <label>
-                Mulighed for tilkøb til turen?
-            </label>
-            <input type="checkbox" value="tilkoeb"/>
-            <h3>Special</h3>
-            <label>Er denne tur en special tur?</label>
-            <input type="checkbox" value="special"/>
-            <p>{errorMessage}</p>
-            <button type="submit">Gem fartplan</button>
-        </form>
-    )
+      </select>
+      <h3>Pax</h3>
+      <label>Antal Pax</label>
+      <input
+        type="number"
+        min="1"
+        max="100"
+        onChange={(e) => setPax(e.target.value)}
+      />
+      <h3>Tilkøb</h3>
+      <label>Mulighed for tilkøb til turen?</label>
+      <input type="checkbox" value="tilkoeb" />
+      <h3>Special</h3>
+      <label>Er denne tur en special tur?</label>
+      <input type="checkbox" value="special" />
+      <p>{errorMessage}</p>
+      <button type="submit">Gem fartplan</button>
+    </form>
+  );
 }
--- src/pages/timetables/TimetableRoute.js
+++ src/pages/timetables/TimetableRoute.js
@@ -1,62 +1,65 @@
 import { doc, getDoc, onSnapshot, query, where } from "@firebase/firestore";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { format } from "date-fns";
 import { useNavigate, useParams } from "react-router-dom";
 import { routesRef, timeTablesRef } from "../../firebaseConfig";
 
-
-export default function TimetableRoute(){
-const [timetables, setTimetables] = useState([]);
-const [selectedRoute, setSelectedRoute] = useState("");
-const params = useParams(); // url parameter
-const routeId = params.routeId; // get route id from url parameter
-const navigate = useNavigate();
-useEffect(() => {
+export default function TimetableRoute() {
+  const [timetables, setTimetables] = useState([]);
+  const [selectedRoute, setSelectedRoute] = useState("");
+  const params = useParams(); // url parameter
+  const routeId = params.routeId; // get route id from url parameter
+  const navigate = useNavigate();
+  useEffect(() => {
     async function getRoute() {
       const docRef = doc(routesRef, routeId); // create post ref based on routeId from url parameter
       const docData = await getDoc(docRef); // get post data (one specific post)
       setSelectedRoute(docData.data().name); // setting post state with data from firestore
     }
     async function getTimeTables() {
-        const q = query(
-          timeTablesRef,
-          where("route", "==", routeId)
-        );
-        const unsubscribe = onSnapshot(q, (data) => {
-          const timetableData = data.docs.map((doc) => {
-            // map through all docs (object) from post collection
-            return { ...doc.data(), id: doc.id }; // changing the data structure so it's all gathered in one object
-          });
-          setTimetables(timetableData);
+      const q = query(timeTablesRef, where("route", "==", routeId));
+      const unsubscribe = onSnapshot(q, (data) => {
+        const timetableData = data.docs.map((doc) => {
+          // map through all docs (object) from post collection
+          return { ...doc.data(), id: doc.id }; // changing the data structure so it's all gathered in one object
         });
-        return () => unsubscribe(); // tell the post component to unsubscribe from listen on changes from firestore
-      }
+        setTimetables(timetableData);
+      });
+      return () => unsubscribe(); // tell the post component to unsubscribe from listen on changes from firestore
+    }
     getRoute();
     getTimeTables();
   }, [routeId]); // called every time routeId changes
 
-  function handleGoBack(){
-        navigate("/fartplaner")
-  };
-function handleNewTimeTable(){
-    navigate(`/fartplaner/${routeId}/nyfartplan`)
-}
-    return(
-        <div className="timetableselect">
-            <button onClick={handleGoBack}>Tilbage</button>
-            <h1>Fartplaner</h1>
-            <h3>Fartplaner for {selectedRoute}</h3>
-            {timetables.map((timetable) => (
-            <div
+  function handleGoBack() {
+    navigate("/fartplaner");
+  }
+  function formatDate(date) {
+    const formattedDate = format(new Date(date), "dd MMMM yyyy");
+    return formattedDate;
+  }
+  function handleNewTimeTable() {
+    navigate(`/fartplaner/${routeId}/nyfartplan`);
+  }
+  return (
+    <div className="timetableselect">
+      <button onClick={handleGoBack}>Tilbage</button>
+      <h1>Fartplaner</h1>
+      <h3>Fartplaner for {selectedRoute}</h3>
+      {timetables.map((timetable) => (
+        <div
           key={timetable.id}
           className="timetable"
           onClick={function () {
             navigate(`/fartplaner/${timetable.id}`);
           }}
         >
-          <h3> {timetable.name} </h3>
+          <h3>
+            {formatDate(timetable.startDate)} - {formatDate(timetable.endDate)}
+          </h3>
         </div>
       ))}
-            <button onClick={handleNewTimeTable}>Ny fartplan</button>
-        </div>
-    )
+      <button onClick={handleNewTimeTable}>Ny fartplan</button>
+    </div>
+  );
 }
