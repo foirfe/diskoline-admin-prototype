deda419aa549586df619d8a3d52ee424452fb887
Daníel Freyr <83401867+foirfe@users.noreply.github.com>
2022-12-08 18:08:16 +0100

    Forgotten password navigation back

--- src/pages/ForgottenPassword.js
+++ src/pages/ForgottenPassword.js
@@ -1,8 +1,18 @@
-export default function ForgottenPassword(){
+import { useNavigate } from "react-router-dom";
 
-    return(
-        <div className="public_page">
-            <h1>Glemt Kode?</h1>
-        </div>
-    )
+export default function ForgottenPassword() {
+  const navigate = useNavigate();
+  function handleNavigate() {
+    navigate("/");
+  }
+  return (
+    <div className="public_page">
+      <h1>Glemt Kode?</h1>
+      <p>
+        Denne funktion virker ikke, men er blot for at vise at her ville man
+        bede om at få nulstillet sin kode
+      </p>
+      <button onClick={handleNavigate}>Tilbage til Login</button>
+    </div>
+  );
 }
--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,15 +1,14 @@
 import SignInForm from "../components/SignInForm";
-import logo from '../images/disko-line-emblem.png';
+import logo from "../images/disko-line-emblem.png";
 
-export default function SignInPage(){
-
-    return(
-        <div className="public_page">
-        <img src={logo} alt="Disko Line" className="signinlogo" />
-        <div className="titleandform">
+export default function SignInPage() {
+  return (
+    <div className="public_page">
+      <img src={logo} alt="Disko Line" className="signinlogo" />
+      <div className="titleandform">
         <h1>Administration</h1>
-        <SignInForm/>
-        </div>
-        </div>
-    )
+        <SignInForm />
+      </div>
+    </div>
+  );
 }
